cf09455687028c87bf6ea304a2f65b6dca27ab53
samiclaros <cla24007@byui.edu>
2024-06-15 18:01:05 -0400

    Finished de JS format

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -5,10 +5,11 @@ currentyear.innerHTML = `<span class="highlight">${today.getFullYear()}</span>`;
 
 const lastModified = document.querySelector("#lastModified");
 const lastModifiedDate = new Date(document.lastModified);
-const formattedDate = lastModifiedDate.toLocaleString("en-US", {
-  year: 'numeric', month: '2-digit', day: '2-digit',
-  hour: '2-digit', minute: '2-digit', second: '2-digit',
-  hour12: false
-});
 
-lastModified.innerHTML = `Last modified: ${formattedDate}`;
+lastModified.innerHTML = `Last modified: <span class="highlight">${new Intl.DateTimeFormat(
+	"en-US",
+	{
+		dateStyle: "short",
+    timeStyle: "medium"
+	}
+).format(lastModifiedDate)}</span>`;
