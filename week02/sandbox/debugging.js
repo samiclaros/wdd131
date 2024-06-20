b4e20e17cbfbaad28d67e52721f3aa8499bc4b93
samiclaros <cla24007@byui.edu>
2024-06-20 14:43:40 -0400

    Debugging practice done

---
+++ week02/sandbox/debugging.js
@@ -0,0 +1,8 @@
+const PI = 3.14;
+radius = 3; // Changed Headers,I take out the const
+let area = 0;
+area = radius * radius * PI; // Changed here, instead of "pi" the correct is "PI"
+console.log(area);
+radius = 4;
+area = radius * radius * PI; // Changed here, instead of "pi" the correct is "PI"
+console.log(area);
