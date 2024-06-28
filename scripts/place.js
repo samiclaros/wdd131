1e726316e173cb430c53e4d3789dc5cb5b08f6b0
samiclaros <cla24007@byui.edu>
2024-06-28 18:22:40 -0400

    Finished the javascript doc

--- scripts/place.js
+++ scripts/place.js
@@ -13,3 +13,19 @@ lastModified.innerHTML = `Last modified: <span class="highlight">${new Intl.Date
 	}
 ).format(lastModifiedDate)}</span>`;
 
+const windSpeed = 12;
+const temperature = 10;
+const windchill = document.querySelector("#windchill");
+
+const windchillOperation = function(temperature, windSpeed){
+    if(temperature <=10 && windSpeed > 4.8)
+        {
+            let windchill = 13.12 + (0.6215*temperature) - (11.37*(windSpeed**0.16)) + (0.3965*temperature*(windSpeed**0.16));
+            return windchill.toFixed(2);
+        }
+    else
+    {
+        return "N/A";
+    }
+}
+windchill.innerHTML = `<span class="highlight">${windchillOperation(temperature, windSpeed)}</span>`;
