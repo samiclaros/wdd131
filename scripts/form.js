3f8b0ba7f2a64397b856488f8b80ccc26b56cd72
samiclaros <cla24007@byui.edu>
2024-07-13 13:33:41 -0400

    Problems to display de number of the review

--- scripts/form.js
+++ scripts/form.js
@@ -1,3 +1,5 @@
+// To get the last modified
+
 const currentyear = document.querySelector("#currentyear");
 const today = new Date();
 currentyear.innerHTML = `<span class="highlight">${today.getFullYear()}</span>`;
@@ -13,7 +15,56 @@ lastModified.innerHTML = `Last modified: <span class="highlight">${new Intl.Date
 	}
 ).format(lastModifiedDate)}</span>`;
 
+// To get the last modified
+
 // CONTENT OF THE LIST OF PRODUCTS
 
+const products = [
+	{
+	  id: "fc-1888",
+	  name: "flux capacitor",
+	  averageRating: 4.5
+	},
+	{
+	  id: "fc-2050",
+	  name: "power laces",
+	  averageRating: 4.7
+	},
+	{
+	  id: "fs-1987",
+	  name: "time circuits",
+	  averageRating: 3.5
+	},
+	{
+	  id: "ac-2000",
+	  name: "low voltage reactor",
+	  averageRating: 3.9
+	},
+	{
+	  id: "jj-1969",
+	  name: "warp equalizer",
+	  averageRating: 5.0
+	}
+];
 
 const productName = document.querySelector("#product-name");
+products.forEach(product => {
+	const option = document.createElement("option");
+	option.value = product.id;
+	option.textContent = product.name;
+	productName.appendChild(option);
+});
+
+// CONTENT OF THE LIST OF PRODUCTS
+
+const reviewAmount = document.querySelector("#review-amount");
+let numReviews = Number(window.localStorage.getItem("numReviews-ls")) || 0;
+
+if (numReviews > 0) { 
+  reviewAmount.textContent = numReviews;
+} else {
+  reviewAmount.textContent = `This is your first review 🤩`;
+}
+
+numReviews++;
+localStorage.setItem("numReviews-ls", numReviews);
