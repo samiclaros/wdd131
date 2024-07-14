6d9a84879738ef077e65b742dc802176d2c4cc33
samiclaros <cla24007@byui.edu>
2024-07-14 16:07:22 -0400

    Finally fixed the number of reviews

--- scripts/review.js
+++ scripts/review.js
@@ -4,12 +4,12 @@ const reviewAmount = document.querySelector("#review-amount");
 let numReviews = Number(window.localStorage.getItem("numReviews-ls")) || 0;
 
 if (numReviews > 0) { 
-  reviewAmount.textContent = numReviews;
+  reviewAmount.textContent = numReviews + 1;
 } else {
   reviewAmount.textContent = `1`;
 }
 
-numReviews++;
+numReviews++ + 1;
 localStorage.setItem("numReviews-ls", numReviews);
 
 // NUMBER OF REVIEWS
