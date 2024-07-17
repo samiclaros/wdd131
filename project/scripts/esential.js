cb4cf0473141920441b7d9a5c7c2d97ba88e55e7
samiclaros <cla24007@byui.edu>
2024-07-17 19:22:06 -0400

    Cant figure out about the hamburguer button

--- project/scripts/esential.js
+++ project/scripts/esential.js
@@ -1,3 +1,13 @@
 const currentyear = document.querySelector("#currentyear");
 const today = new Date();
 currentyear.innerHTML = `<span class="highlight">${today.getFullYear()}</span>`;
+
+// HAM BUTTON
+
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.top-nav');
+
+hamButton.addEventListener('click', () => {
+	navigation.classList.toggle('open');
+	hamButton.classList.toggle('open');
+});
